f3a1b1b9a8c8860453948c338028bfdc8ecd56ea
DuyHuynh <duyhuynhstdio@gmail.com>
2022-09-23 15:58:47 +0700

    INT-68-69

--- src/_metronic/helpers/format/number.ts
+++ src/_metronic/helpers/format/number.ts
@@ -1,5 +1,5 @@
 export const showTwoDecimalPlaces = (number) => {
     if (number && !isNaN(number)) {
-        return (Math.round(number * 100) / 100)?.toFixed(2)
+        return number?.toString()?.match(/^-?\d+(?:\.\d{0,2})?/)?.[0]
     } else return number
 }
